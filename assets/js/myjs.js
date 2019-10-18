1f2a200c2bc1bb1e43294e7eeb65eef49194b376
phanson <phansondhvcntt@gmail.com>
2019-10-18 14:23:25 +0700

    init file

---
+++ assets/js/myjs.js
@@ -0,0 +1,67 @@
+ $('#autoplay').slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerPadding: 3,
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+
+  $('#play').slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay:false,
+    autoplaySpeed: 1000,
+    centerPadding: 3,
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
